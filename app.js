2b631fac3628c3ac296eb16e6467b8c3b540c48b
rukaterin <maylessamurai@yandex.ru>
2024-05-16 18:07:19 +0300

    new project

---
+++ app.js
@@ -0,0 +1,22 @@
+const mainRouteController = require("./controllers/main");
+const defaultRouteController = require("./controllers/default");
+const gameRouteController = require("./controllers/game");
+const voteRouteController = require("./controllers/vote");
+const http = require('http');
+const server = http.createServer((req, res) => {
+    const url = req.url;
+    switch (url) {
+      case "/":
+        mainRouteController(res, "/index.html", ".html");
+        break;
+      case "/game":
+        gameRouteController(res);
+        break;
+        case "/vote":
+          voteRouteController(req, res);
+        break;
+        default:
+          defaultRouteController(res, url);
+    }
+  });
+server.listen(3005);
