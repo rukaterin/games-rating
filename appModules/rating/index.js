a1ab7e3c142acdb71b072e14901f1c32bb334d31
rukaterin <maylessamurai@yandex.ru>
2024-05-18 15:49:37 +0300

    update rating/index.js

--- appModules/rating/index.js
+++ appModules/rating/index.js
@@ -1,7 +1,6 @@
 const config = require("./config");
 const makeRatingFile = require("./rating-file");
-const createRating = require("./calculations");
-const updateRating = require("./calculations");
+const {createRating, updateRating} = require("./calculations");
 
 module.exports = {
     config,
